e636776bb39624b1aa96c24c3792e4151c25a0df
Jia-Yi Jian <jyjian@illinois.edu>
2023-08-14 15:25:30 -0700

    add profile main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+const profile = {
+  name: "Jessie",
+  imageUrl: '',
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <h1>{profile.name}</h1>
+        <img
+        src={profile.imageUrl}
+        alt={`${profile.name}'s profile picture`}
+        ></img>
+    </>
+);
 }
 
 export default App;

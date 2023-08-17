ed95fe48547fedba8806684203cfae84e4c6b63f
Jia-Yi Jian <jyjian@illinois.edu>
2023-08-16 17:47:56 -0700

    created projects list

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,52 @@
 function Projects(){
-    return <>This is a test</>
+
+    const projectsList = [
+        {
+            picture: '',
+            name: "Buy it for life",
+            description: "A Full Stack web applcation",
+            tag: ["HTML", "CSS", "Bootstrap", "React.js", "Node.js","Express.js", "MongoDB" ]
+        },
+        {
+            picture: '',
+            name: "ID Photo Generator",
+            description: "An website that for user to generate a qualified ID photo. I am responsible for the Frontend design and make it responsible for different viewports",
+            tag: ["HTML", "CSS", "Bootstrap" ]
+        },
+        {
+            picture: '',
+            name: "Unknown",
+            description: "unknown",
+            tag: ["HTML", "CSS", "Bootstrap", "React.js", "Node.js","Express.js", "MongoDB"]
+        },
+        {
+            picture: '',
+            name: "To-do List",
+            description: "Unknown",
+            tag: ["HTML", "CSS", "Node.js"]
+        },
+        {
+            picture: '',
+            name: "University of Illinois VR Lab Game",
+            description: "Unknown",
+            tag: ["C#", "Unity", "User Experience", "User Interface"]
+        },
+        {
+            picture: '',
+            name: "InstaFood",
+            description: "A full-stack website merging Instagram's friend-following features with Yelp's restaurant search capabilities",
+            tag: ["HTML", "CSS", "Bootstrap", "React.js", "Node.js","Express.js", "MongoDB"]
+        }
+
+    ]
+
+    return (
+    <>
+    <div id="project-cards">
+
+    </div>
+    </>
+    )
 }
 
 export default Projects;

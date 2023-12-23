a54a44f55a6c74c46c4bbe37aac0ab9c7d70f4a7
Jia-Yi Jian <jiayi.jian95@gmail.com>
2023-12-22 17:48:20 -0800

    created and added styles to basket and dropped elements

---
+++ src/components/goldenGame/goldenGame.js
@@ -0,0 +1,11 @@
+function goldenGame () {
+    var game = document.querySelector(".game")
+    var basket = document.querySelector(".basket")
+    var fruits = document.querySelector(".fruits")
+    var basketLeft = parseInt(window.getComputedStyle(basket).getPropertyValue("left"))
+    var basketBottom = parseInt(window.getComputedStyle(basket).getPropertyValue("bottom"))
+
+    
+}
+
+export default goldenGame;

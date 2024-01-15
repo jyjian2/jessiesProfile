7cef3ef550e1a821421014f17394c34f9b2aa2af
Jia-Yi Jian <jiayi.jian95@gmail.com>
2024-01-14 17:53:32 -0800

    adjusted each section's view port height

--- src/components/goldenGame/goldenGame.js
+++ src/components/goldenGame/goldenGame.js
@@ -1,42 +1,4 @@
 function goldenGame () {
-    // var game = document.querySelector(".game")
-    // var basket = document.querySelector(".basket")
-    // var fruits = document.querySelector(".fruits")
-    // var basketLeft = parseInt(window.getComputedStyle(document.getElementByClassName("left"), ""))
-    // var basketBottom = parseInt(window.getComputedStyle(document.getElementByClassName("bottom"), ""))
-
-    // function moveBasketLeft() {
-    //     if (basketLeft > 0) {
-    //         basketLeft -= 15
-    //         basket.style.left = basketLeft + "px"
-    //     }
-    // }
-
-    // function moveBasketRight() {
-    //     if (basket < window.innerWidth) {
-    //         basketLeft += 15
-    //         basket.style.left = basketLeft + "px"
-    //     }
-    // }
-
-    // function control(e) {
-    //     if (e.key == "Arrowleft") {
-    //         moveBasketLeft();
-    //     }
-    //     if (e.key == "ArrowRight") {
-    //         moveBasketRight();
-    //     }
-    // }
-
-    // function generateFruits() {
-    //     var fruit = document.createElement('div')
-    //     fruit.setAttribute("class", "fruit")
-    //     fruits.appendChild(fruit)
-    // }
-
-    
-    // generateFruits();
-    // document.addEventListener("keydown", control);
     return (
         <div>
             TEST

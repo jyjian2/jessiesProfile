65ecd196176e6958512d73fcfe888fa086a0d150
Jia-Yi Jian <jiayi.jian95@gmail.com>
2024-01-14 21:18:41 -0800

    added image to game section with size adjustment in css file

--- src/components/goldenGame/goldenGame.js
+++ src/components/goldenGame/goldenGame.js
@@ -1,10 +1,15 @@
 import { motion } from "framer-motion";
+import tennisBall from "/Users/keziah/Developer/jessies_profile/src/assets/tennisBall.png";
+import './goldenGameStyles.css';
 
-function goldenGame () {
+function goldenGame() {
     return (
         <>
-        
-        
+            <div className='tennisWrap'>
+                <img src={tennisBall} alt="Tennis Ball" />
+            </div>
+
+
         </>
     )
 

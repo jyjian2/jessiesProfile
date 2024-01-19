d909a0317de37a9d5a0478c595f77df3512fcbcb
Jia-Yi Jian <jiayi.jian95@gmail.com>
2024-01-18 17:28:17 -0800

    added seven tennis balls to the game section

--- src/components/goldenGame/goldenGame.js
+++ src/components/goldenGame/goldenGame.js
@@ -5,34 +5,89 @@ import { useState, useEffect } from "react";
 
 const GoldenGame = () => {
     const [isBouncing, setBouncing] = useState(true);
+    const balls = [1, 2, 3, 4, 5, 6, 7, 8];
 
     const ballVariants = {
-        initial: { y: 100 },
-        bounce: { y: [-100, 0, -90, 0, -80, 0, -70, 0, -30, 0], transition: { duration: 10 } },
+        initial: { y: 0 },
+        bounce: { y: [-100, 0, -90, 0, -80, 0, -70, 0, -30, 0], 
+        transition: { duration: 10, repeat: Infinity } },
     };
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setBouncing(false);
-            setTimeout(() => setBouncing(true), 10);
-        }, 2000); // Adjust the interval based on your preference
+        // const interval = setInterval(() => {
+        //     setBouncing(false);
+        //     setTimeout(() => setBouncing(true), 10);
+        // }, 10000); // Adjust the interval based on your preference
 
-        return () => clearInterval(interval);
+        // return () => clearInterval(interval);
     }, []);
 
     return (
-        <div style={{ width: '100%', height: '100vh', display: 'flex', alignItems: 'flex-end' }}>
+        <div style={{ width: '100%', height: '100vh', display: 'flex', alignItems: 'flex-end', justifyContent: 'space-around' }}>
             <motion.img
-                src={tennisBall}
-                animate={isBouncing ? 'bounce' : undefined}
-                variants={ballVariants}
-                style={{
-                    width: 50,
-                    height: 50,
-                }}
-            />
+                    src={tennisBall}
+                    animate={isBouncing ? 'bounce' : undefined}
+                    variants={ballVariants}
+                    style={{
+                        width: 50,
+                        height: 50,
+                        
+                    }}
+                />
+                <motion.img
+                    src={tennisBall}
+                    animate={isBouncing ? 'bounce' : undefined}
+                    variants={ballVariants}
+                    style={{
+                        width: 50,
+                        height: 50,
+                        
+                    }}
+                />
+                <motion.img
+                    src={tennisBall}
+                    animate={isBouncing ? 'bounce' : undefined}
+                    variants={ballVariants}
+                    style={{
+                        width: 50,
+                        height: 50,
+                        
+                    }}
+                />
+                <motion.img
+                    src={tennisBall}
+                    animate={isBouncing ? 'bounce' : undefined}
+                    variants={ballVariants}
+                    style={{
+                        width: 50,
+                        height: 50,
+                        
+                    }}
+                />
+                <motion.img
+                    src={tennisBall}
+                    animate={isBouncing ? 'bounce' : undefined}
+                    variants={ballVariants}
+                    style={{
+                        width: 50,
+                        height: 50,
+                        
+                    }}
+                />
+                <motion.img
+                    src={tennisBall}
+                    animate={isBouncing ? 'bounce' : undefined}
+                    variants={ballVariants}
+                    style={{
+                        width: 50,
+                        height: 50,
+                        
+                    }}
+                />
         </div>
+        
     );
+
 };
 
 

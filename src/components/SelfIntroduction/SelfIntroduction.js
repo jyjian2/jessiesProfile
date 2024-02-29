b3676cbbeb9fe4fbbc22678b19bea4780a7a2ddb
Jia-Yi Jian <jiayi.jian95@gmail.com>
2024-02-28 18:08:04 -0800

    added porfile image to the website

--- src/components/SelfIntroduction/SelfIntroduction.js
+++ src/components/SelfIntroduction/SelfIntroduction.js
@@ -1,19 +1,20 @@
 import CitySkyline from "../CitySkyline/CitySkyline";
+import ProfileImg from "../../assets/jyjian2Profile.jpg";
 
 function SelfIntroduction() {
     const profile = {
         name: "Jessie",
-        imageUrl: '',
+        imageUrl: ProfileImg,
         selfIntro: 'Hi, I am Jessie, a Web Developer with 3+ years of experience creating user-friendly websites, proficient in a modern full-stack toolkit including HTML, CSS, JavaScript, Bootstrap, React, Node, Express, and MongoDB. Proven success collaborating with cross-functional teams, such as development, UI/UX, and management, to provide exceptional web solutions.'
     }
 
     return (
         <div style={{ position: 'relative', width: '100%', height: '100%' }}>
             <CitySkyline />
-            <div style={{ position: 'relative',width: '100%', height: '100%' }}>
+            <div style={{ position: 'relative', width: '100%', height: '100%' }}>
                 <h1>{profile.name}</h1>
                 <img
-                    src={profile.imageUrl}
+                    src={profile.imageUrl} // Use profile.imageUrl as the source
                     alt={`${profile.name}'s profile picture`}
                 ></img>
                 <p>{profile.selfIntro}</p>

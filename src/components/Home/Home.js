723c613270c674568e5cc68e574856f1cdf32b8c
Jia-Yi Jian <jiayi.jian95@gmail.com>
2024-03-11 17:19:26 -0700

    added name and nac bar contents

---
+++ src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import ProfileImg from "../../assets/jyjian2Profile.jpg";
+import "./HomeStyles.css"
+
+function Home() {
+    return (
+        <>
+            <div>
+                <h1>JIA-YI JIAN</h1>
+                <h4>Full Stack Engineer</h4>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#/resume"><p>Resume</p></a></li>
+                    <li><a href="#/resume"><p>Projects</p></a></li>
+                    <li><a href="#/resume"><p>Personal</p></a></li>
+                </ul>
+            </nav>
+
+
+        </>
+    )
+}
+
+export default Home;

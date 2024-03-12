219409440863ed0db3b50518809e485642e325b2
Jia-Yi Jian <jiayi.jian95@gmail.com>
2024-03-12 14:46:27 -0700

    completed header layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,33 @@
 import ProfileImg from "../../assets/jyjian2Profile.jpg";
 import "./HomeStyles.css"
+import { Col, Row, Container } from 'react-bootstrap';
 
 function Home() {
     return (
         <>
-            <div>
-                <h1>JIA-YI JIAN</h1>
-                <h4>Full Stack Engineer</h4>
-            </div>
-            <div className="nav-bar">
-                <nav>
-                    <ul>
-                        <li><a href="#/resume"><p>Resume</p></a></li>
-                        <li><a href="#/resume"><p>Projects</p></a></li>
-                        <li><a href="#/resume"><p>Personal</p></a></li>
-                    </ul>
-                </nav>
-            </div>
+
+            <Row>
+                <Col xs="12" md="6">
+                    <h1 className="name">JIA-YI JIAN</h1>
+                    <h4 className="title">Full Stack Engineer</h4>
+                </Col>
+                <Col xs="12" md="6">
+
+                    <Row className="nav-bar">
+                        <Col s="2"><a href="#/resume"><h4 className="nav-item">Resume</h4></a></Col>
+                        <Col s="2"><a href="#/resume"><h4 className="nav-item">Projects</h4></a></Col>
+                        <Col s="2"><a href="#/resume"><h4 className="nav-item">Personal</h4></a></Col>
+                    </Row>
+
+                </Col>
+
+            </Row>
+
 
 
 
         </>
+
     )
 }
 

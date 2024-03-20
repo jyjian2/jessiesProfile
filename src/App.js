b30d5e4dbef42a4f48e693d45f960b4717e016da
Jia-Yi Jian <jiayi.jian95@gmail.com>
2024-03-19 18:25:31 -0700

    before adding resume content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home.js'; 
 import Layout from './components/Layout/Layout.js'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Resume from './components/Resume'; 
+import Resume from './components/Resume/Resume.js'; 
 // import Projects from './components/Projects'; 
 // import Personal from './components/Personal'; 
 
@@ -12,8 +12,8 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
       <Route index element={<Home />} />
-      {/* <Route path="resume" element={<Resume />} />
-      <Route path="projects" element={<Projects />} />
+      <Route path="resume" element={<Resume />} />
+      {/* <Route path="projects" element={<Projects />} />
       <Route path="personal" element={<Personal />} /> */}
       </Route>
     </Routes>
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,5 +1,59 @@
+import "./ResumeStyles.css"
+import { Col, Row, Container } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function Resume() {
-    return <>This is a test</>
+    return (
+        <>
+
+            <div className="space"></div>
+            <div className="resume-wrap">
+                <Row >
+                    <Col>
+                        <p>test</p>
+                    </Col>
+                    <Col md="6" xs="12" ><p>Self Inttro. dfjdkgjfklgjlsgjlfdgjslfjglsjglf
+                        ggslkfjgldjglfdgjlsjg'sgjldjgdlfgjsklfgj
+                        slgjlfkjgdklfjgfkghjifgjodlfjgldfjgflkgj kfgjldgjlf ggslkfjgldjglfdgjlsjg
+                        gjkfsldjglfkjdglfjgklfgjlfgjlfgjldjg</p>
+                    </Col>
+                </Row>
+                <Row >
+                    <Col>
+                    <p>test</p>
+                    </Col>
+                    <Col md="6" xs="12" ><p>Self Inttro. dfjdkgjfklgjlsgjlfdgjslfjglsjglf
+                        ggslkfjgldjglfdgjlsjg'sgjldjgdlfgjsklfgj
+                        slgjlfkjgdklfjgfkghjifgjodlfjgldfjgflkgj kfgjldgjlf ggslkfjgldjglfdgjlsjg
+                        gjkfsldjglfkjdglfjgklfgjlfgjlfgjldjg</p>
+                    </Col>
+                </Row>
+                <Row >
+                    <Col>
+                    <p>test</p>
+                    </Col>
+                    <Col md="6" xs="12" ><p>Self Inttro. dfjdkgjfklgjlsgjlfdgjslfjglsjglf
+                        ggslkfjgldjglfdgjlsjg'sgjldjgdlfgjsklfgj
+                        slgjlfkjgdklfjgfkghjifgjodlfjgldfjgflkgj kfgjldgjlf ggslkfjgldjglfdgjlsjg
+                        gjkfsldjglfkjdglfjgklfgjlfgjlfgjldjg</p>
+                    </Col>
+                </Row>
+            </div>
+
+
+
+
+
+
+
+
+
+
+        </>
+
+    )
 }
 
 export default Resume;

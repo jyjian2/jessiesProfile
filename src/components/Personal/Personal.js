65af90f8e1b7dda29c8ac73d451c7b890edecfe1
Jia-Yi Jian <jiayi.jian95@gmail.com>
2024-03-25 16:23:28 -0700

    moved cityView as a new component

--- src/components/Personal/Personal.js
+++ src/components/Personal/Personal.js
@@ -1,20 +1,43 @@
 import { Col, Row, Container, Stack } from 'react-bootstrap';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './PersonalStyles.css';
+import CityView from '/Users/keziah/Developer/jessies_profile/src/components/NewCityView/NewCityView.js';
 
 
 function Personal() {
     return (
         <>
             <div className="space"></div>
-            <div style={{paddingLeft: "10vw", paddingRight: "3vw"}}>
+
+            <div style={{ paddingLeft: "12vw", paddingRight: "3vw" }}>
                 <Row >
                     <Col md="5" className="d-flex align-items-center justify-content-center">
+                        Nostra natoque ultricies augue. Justo lacinia molestie. Lacinia leo nonummy eget duis at mollis orci aliquet rhoncus nunc pretium. Tristique pede tempor dui lacus odio Facilisi ultrices sed morbi tempus dis cursus pulvinar laoreet cum consectetuer suscipit habitasse nibh montes ultrices.
+                        Proin lobortis class nisi natoque vitae, ridiculus auctor felis blandit fusce proin. Ac nullam euismod nam ipsum ante dignissim integer, cubilia.
+                        Augue luctus consectetuer rhoncus class tristique habitasse leo tortor natoque lorem netus eget elit arcu litora. Tellus leo laoreet commodo magnis nullam fringilla facilisis nec arcu nascetur vestibulum pretium faucibus rhoncus senectus torquent leo. Dui torquent purus integer.
                     </Col>
 
                     <Col md="7" xs="12" >
+
+
                     </Col>
                 </Row>
+                <div className="personal-divider"></div>
+            </div>
+            <div style={{ paddingLeft: "12vw", paddingRight: "3vw" }}>
+                <Row >
+                    <Col md="5" className="d-flex align-items-center justify-content-center">
+                        Nostra natoque ultricies augue. Justo lacinia molestie. Lacinia leo nonummy eget duis at mollis orci aliquet rhoncus nunc pretium. Tristique pede tempor dui lacus odio Facilisi ultrices sed morbi tempus dis cursus pulvinar laoreet cum consectetuer suscipit habitasse nibh montes ultrices.
+                        Proin lobortis class nisi natoque vitae, ridiculus auctor felis blandit fusce proin. Ac nullam euismod nam ipsum ante dignissim integer, cubilia.
+                        Augue luctus consectetuer rhoncus class tristique habitasse leo tortor natoque lorem netus eget elit arcu litora. Tellus leo laoreet commodo magnis nullam fringilla facilisis nec arcu nascetur vestibulum pretium faucibus rhoncus senectus torquent leo. Dui torquent purus integer.
+                    </Col>
+
+                    <Col md="7" xs="12" >
+                        <CityView />
+                    </Col>
+                </Row>
+                <div className="personal-divider"></div>
             </div>
 
         </>

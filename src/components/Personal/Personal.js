9d5362742f9a40d19eca00b8e6767660105e854f
Jia-Yi Jian <jiayi.jian95@gmail.com>
2024-03-25 14:26:01 -0700

    layout personal component

--- src/components/Personal/Personal.js
+++ src/components/Personal/Personal.js
@@ -1,5 +1,24 @@
+import { Col, Row, Container, Stack } from 'react-bootstrap';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function Personal() {
-    return <>This is a test</>
+    return (
+        <>
+            <div className="space"></div>
+            <div style={{paddingLeft: "10vw", paddingRight: "3vw"}}>
+                <Row >
+                    <Col md="5" className="d-flex align-items-center justify-content-center">
+                    </Col>
+
+                    <Col md="7" xs="12" >
+                    </Col>
+                </Row>
+            </div>
+
+        </>
+    )
 }
 
 export default Personal;

5d7b3b35adcc32b0b7b4e979037cfb7d21daf0c2
Jia-Yi Jian <jiayi.jian95@gmail.com>
2024-04-02 16:51:53 -0700

    added list of summary in the homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,10 +16,17 @@ function Home() {
                     <Col className="picture">
                         <img src={ProfileImg} style={{ borderRadius: '50%', width: '40vw', height: 'auto' }}></img>
                     </Col>
-                    <Col md="6" xs="12" className="intro"><p>Self Inttro. dfjdkgjfklgjlsgjlfdgjslfjglsjglf
-                        ggslkfjgldjglfdgjlsjg'sgjldjgdlfgjsklfgj
-                        slgjlfkjgdklfjgfkghjifgjodlfjgldfjgflkgj kfgjldgjlf ggslkfjgldjglfdgjlsjg
-                        gjkfsldjglfkjdglfjgklfgjlfgjlfgjldjg</p>
+                    <Col md="7" xs="12" className="intro">
+                        <ul>
+                            <li>Experienced web developer, specializing in JavaScript, HTML/CSS, and Git.</li>
+                            <li>Skilled in building robust web applications using frameworks such as React.js and Node.js.</li>
+                            <li>Proficient in database management with PostgreSQL, MySQL, and MongoDB.</li>
+                            <li>Excels in creating intuitive user interfaces and optimizing performance for engaging user experiences.</li>
+                            <li>Experienced in project management tools like JIRA and design tools like Figma.</li>
+                            <li>Proven track record delivering high-quality software, including AI recipe management and e-commerce solutions</li>
+                            <li>Completed a Master of Science in Information Management from the University of Illinois at Urbana-Champaign.</li>
+                            <li>Greencard holder, don't need sponsordhip</li>
+                        </ul>
                     </Col>
                 </Row>
             </div>
